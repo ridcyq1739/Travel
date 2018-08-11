4e7db09076b8f8d39474e67ab1cfdf5f26ae9493
ridcyq1739 <ridcyq@gmail.com>
2018-08-11 22:53:34 +0800

    'finish-page'

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -17,12 +17,11 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState } from 'vuex'
 export default{
   name: 'HomeHeader',
   computed: {
-    ...mapState(['city']),
-    ...mapGetters(['doubleCity'])
+    ...mapState(['city'])
   }
 }
 </script>
